73dc601ba9b69eedaabb33ef077bffa4bfa6d39d
Lynn <lynn@foldr.moe>
2021-05-11 17:02:08 +0200

    detect kango

--- accent.py
+++ accent.py
@@ -1,9 +1,22 @@
 # Drop this file next to me:
 # https://raw.githubusercontent.com/javdejong/nhk-pronunciation/master/ACCDB_unicode.csv
+# https://raw.githubusercontent.com/davidluzgouveia/kanji-data/master/kanji.json
 
-import csv
 from collections import defaultdict
 from dataclasses import dataclass
+from jaconv import hira2kata
+from os import path
+import csv
+import itertools
+import json
+import sys
+import unicodedata
+
+if not path.exists('kanji-data/kanji.json'):
+    sys.exit('Run `git submodule update --init` first.')
+
+with open('kanji-data/kanji.json') as f:
+    onyomi = {k: v['readings_on'] for k, v in json.load(f).items()}
 
 @dataclass
 class Mora:
@@ -31,13 +44,26 @@ def accent_number(morae):
     '''Return 0 for heiban, 1 for atamadaka, etc.'''
     return 1 + ''.join(m.pitch for m in morae).find('2')
 
+def normalize_reading(kana):
+    return unicodedata.normalize('NFC', kana).translate({
+        'ッ': 'ツ',
+        '\N{combining katakana-hiragana voiced sound mark}': '',
+        '\N{combining katakana-hiragana semi-voiced sound mark}': '',
+    })
+
+def predict_kango_readings(kanji):
+    ons = (onyomi.get(k, []) for k in kanji)
+    return (hira2kata(''.join(t)) for t in itertools.product(*(ons)))
+
 readings = defaultdict(list)
-with open('ACCDB_unicode.csv') as f:
+with open('nhk-pronunciation/ACCDB_unicode.csv') as f:
     for row in csv.reader(f):
         kanji = row[6]
         kana = row[-4]
         accent = row[-1].zfill(len(kana))
         morae = to_morae(kana, accent)
-        print(kanji, morae, accent_number(morae))
+        # print(kanji, morae, accent_number(morae))
+        if normalize_reading(kana) in map(normalize_reading, predict_kango_readings(kanji)):
+            print('Kango:', kanji, kana)
         readings[kanji].append(morae)
 
